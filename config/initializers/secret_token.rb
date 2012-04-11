cc9750b364d8f06217d7ce33632e0b470c4bf203
sionc <sion.chaudhuri@gmail.com>
2012-04-11 10:16:52 -0700

    Adding rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ace::Application.config.secret_token = '6156b9848a755197777924d5d0a10516677cf0f685737326081ddb9e2503dff539e7bec59e8ff3f3b621585900266314eba8b707dc86aa0f0ff9bf179b8fd48e'
